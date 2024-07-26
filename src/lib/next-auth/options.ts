606547465e253a0f98f1e094ba34ab3f35d84cc9
Kodai <0711dr5@gmail.com>
2024-07-26 17:06:51 +0900

    fix:nextAuthOptions作成

---
+++ src/lib/next-auth/options.ts
@@ -0,0 +1,28 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const prisma = new PrismaClient();
+
+export const nextAuthOptions: NextAuthOptions = {
+  debug: false,
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: async ({ session, user }) => {
+      return {
+        ...session,
+        user: {
+          id: user.id,
+          ...session.user,
+        },
+      };
+    },
+  },
+};
