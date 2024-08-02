030b97db7ebc81553a7d0bd8e893db3dfb9279ed
Kodai <0711dr5@gmail.com>
2024-08-02 17:42:12 +0900

    fix:記事取得関数作成

--- src/components/book.tsx
+++ src/components/book.tsx
@@ -28,7 +28,7 @@ const Book = ({ book }: any) => {
         <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
           <Image
             priority
-            src={book.thumbnail}
+            src={book.thumbnail.url}
             alt={book.title}
             width={450}
             height={350}
@@ -36,7 +36,9 @@ const Book = ({ book }: any) => {
           />
           <div className="px-4 py-4 bg-slate-100 rounded-b-md">
             <h2 className="text-lg font-semibold">{book.title}</h2>
-            <p className="mt-2 text-lg text-slate-600">この本は○○...</p>
+            <p className="mt-2 text-lg text-slate-600 line-clamp-3">
+              {book.content}
+            </p>
             <p className="mt-2 text-md text-slate-700">値段：{book.price}円</p>
           </div>
         </a>
