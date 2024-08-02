0ea0076e83327b92cd49c7f118729bb841e4e271
Kodai <0711dr5@gmail.com>
2024-08-02 18:28:12 +0900

    fix:logout機能追加

--- src/components/book.tsx
+++ src/components/book.tsx
@@ -1,10 +1,38 @@
 "use client";
 
-import { BookType } from "@/types/types";
 import Image from "next/image";
+import { memo, useState } from "react";
+import { BookType } from "../types/types";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+type BookProps = {
+  book: BookType;
+};
 
 // eslint-disable-next-line react/display-name
-const Book = ({ book }: { book: BookType }) => {
+const Book = memo(({ book }: BookProps) => {
+  const { data: session } = useSession();
+  const user = session?.user;
+  const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
+
+  const handlePurchaseClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCancel = () => {
+    setShowModal(false);
+  };
+
+  const handleperchaseConfirm = () => {
+    if (!user) {
+      setShowModal(false);
+      router.push("/login");
+    } else {
+    }
+  };
+
   return (
     <>
       {/* アニメーションスタイル */}
@@ -25,7 +53,10 @@ const Book = ({ book }: { book: BookType }) => {
       `}</style>
 
       <div className="flex flex-col items-center m-4">
-        <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+        <a
+          onClick={handlePurchaseClick}
+          className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none"
+        >
           <Image
             priority
             src={book.thumbnail.url}
@@ -36,27 +67,32 @@ const Book = ({ book }: { book: BookType }) => {
           />
           <div className="px-4 py-4 bg-slate-100 rounded-b-md">
             <h2 className="text-lg font-semibold">{book.title}</h2>
-            <p className="mt-2 text-lg text-slate-600 line-clamp-3">
-              {book.content}
-            </p>
+            <p className="mt-2 text-lg text-slate-600">この本は○○...</p>
             <p className="mt-2 text-md text-slate-700">値段：{book.price}円</p>
           </div>
         </a>
-
-        {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
-          <div className="bg-white p-8 rounded-lg">
-            <h3 className="text-xl mb-4">本を購入しますか？</h3>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-              購入する
-            </button>
-            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              キャンセル
-            </button>
+        {showModal && (
+          <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
+            <div className="bg-white p-8 rounded-lg">
+              <h3 className="text-xl mb-4">本を購入しますか？</h3>
+              <button
+                onClick={handleperchaseConfirm}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+              >
+                購入する
+              </button>
+              <button
+                onClick={handleCancel}
+                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+              >
+                キャンセル
+              </button>
+            </div>
           </div>
-        </div> */}
+        )}
       </div>
     </>
   );
-};
+});
 
 export default Book;
